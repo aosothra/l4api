36a092d53ef962b26c96a84746f1f57fcf9fc7e2
aosothra <arhdam@yandex.ru>
2022-03-08 17:57:07 +0400

    Formated in accordance with PEP8

--- fetch_nasa.py
+++ fetch_nasa.py
@@ -16,8 +16,8 @@ def fetch_nasa_apod(api_key, img_count=30):
     apod_api = 'https://api.nasa.gov/planetary/apod'
 
     payload = {
-        'count':img_count,
-        'api_key':api_key
+        'count': img_count,
+        'api_key': api_key
     }
 
     response = requests.get(apod_api, params=payload)
@@ -25,7 +25,7 @@ def fetch_nasa_apod(api_key, img_count=30):
 
     apods = response.json()
     for i, apod in enumerate(apods):
-        if apod['media_type']=='image':
+        if apod['media_type'] == 'image':
             ext = get_file_extension(apod['url'])
             img_name = f'image_apod_{i}{ext}'
             img_data = fetch_image(apod['url'])
@@ -33,11 +33,14 @@ def fetch_nasa_apod(api_key, img_count=30):
 
 
 def fetch_nasa_epic_image(api_key, img_name, date, index):
-    formated_date = date.strftime('%Y/%m/%d') 
-    url = f'https://api.nasa.gov/EPIC/archive/natural/{formated_date}/png/{img_name}.png'
+    formated_date = date.strftime('%Y/%m/%d')
+    url = (
+        f'https://api.nasa.gov/EPIC/archive/natural/'
+        f'{formated_date}/png/{img_name}.png'
+        )
 
     payload = {
-        'api_key':api_key
+        'api_key': api_key
     }
 
     img_name = f'image_epic_{index}.png'
@@ -49,7 +52,7 @@ def fetch_nasa_epic_imgset(api_key):
     url = 'https://api.nasa.gov/EPIC/api/natural/images'
 
     payload = {
-        'api_key':api_key
+        'api_key': api_key
     }
 
     response = requests.get(url, params=payload)
